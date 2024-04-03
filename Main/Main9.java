1eedd2ee6455078d9bea131168b366317250b6c1
Kim Sujeong <krystal823@naver.com>
2024-04-03 23:42:55 +0900

    buffer

---
+++ Main/Main9.java
@@ -0,0 +1,18 @@
+public static void main9(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String[] input = br.readLine().split(" ");
+        
+    int A = Integer.parseInt(input[0]);
+    int B = Integer.parseInt(input[1]);
+    int C = Integer.parseInt(input[2]);
+    int[] s = {A, B, C};
+    Arrays.sort(s);
+        
+    if(s[0]+s[1] > s[2]){
+        System.out.println(A+B+C);
+    }else{
+        System.out.println((s[0]+s[1]) * 2 - 1);
+    }
+        
+    br.close();
+}
