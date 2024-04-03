4915835c9ebe2f38215ddf886260d3fad3407e71
Kim Sujeong <krystal823@naver.com>
2024-04-03 23:46:23 +0900

    buffer

---
+++ Main/Main10.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main10 {
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int sum = factorial(N);
+		System.out.println(sum);
+		
+	}
+	
+	public static int factorial(int N) {
+		if(N <= 1) return 1;	// 재귀 종료조건
+		return N * factorial(N - 1);		
+	}
+ 
+}
